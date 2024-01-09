c5a8bf25151291c2e8602a03bb90bd821d5e4baa
Aleksey Grushenkov <153802645+Grugrusha96@users.noreply.github.com>
2024-01-09 16:49:45 +0300

    arraysorting

---
+++ src/arraysorting.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+// 5. Создайте двумерный массив целых чисел.
+// Отсотрируйте элементы в строках двумерного массива по возрастанию.
+public class arraysorting {
+    public static void main (String[] args){
+        int size = 9;
+        int [][] array = new int[size][size];
+        for (int i = 0; i< array.length; i++){
+            for (int j = 0; j< array.length; j++){
+                array[i][j] = (int) (Math.random() * 10);
+                System.out.print (array[i][j] + " ");
+            }
+            System.out.println();
+        }
+        for (int[] sort : array){
+        }
+        System.out.println(Arrays.deepToString(array));
+    }
+}
